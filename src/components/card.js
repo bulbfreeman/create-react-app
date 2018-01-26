9701345d56f3ea5dc8678e230272630f0d8915e5
bulbfreeman <bulbfreeman@users.noreply.github.com>
2018-01-26 18:30:07 +0800

    Update Card

--- src/components/card.js
+++ src/components/card.js
@@ -5,8 +5,17 @@ import Card, { CardActions, CardContent } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import SimpleSelect from './cardSelector';
+import Table from 'material-ui/Table/Table';
+import TableRow from 'material-ui/Table/TableRow';
+import TableCell from 'material-ui/Table/TableCell';
+import TableBody from 'material-ui/Table/TableBody';
+import CardHeader from 'material-ui/Card/CardHeader';
 
 const styles = theme => ({
+  table: {
+    maxWidth: 270,
+    paddingLeft: 0,
+  },
   card: {
     maxWidth: 310,
   },
@@ -18,20 +27,20 @@ class SimpleCard extends React.Component {
 
   state = {
     date: '',
-    status: 'Not Available',
+    status: 'N/A',
     total: 'N/A',
-    newBk: 'N/A',
+    newBk: 'Not Available',
   };
 
   onDateChange = (dt) => {
     this.setState({date: dt});
 
-    fetch(baseAPI+`summary/MDS/`+dt)
+    fetch(baseAPI+`summary/`+this.props.sysName+`/`+dt)
 		.then(r=>r.json())
 		.then(data=>this.setState({total: data.total, newBk: data.new}))
     .catch(this.setState({total: 'N/A', newBk: 'N/A'}));
     
-		fetch(baseAPI+`status/MDS/`+dt)
+		fetch(baseAPI+`status/`+this.props.sysName+`/`+dt)
 		.then(r=>r.json())
 		.then(data=>this.setState({status: data.status}))
 		.catch(this.setState({status: 'Not Available'}));
@@ -52,12 +61,21 @@ class SimpleCard extends React.Component {
         <Card className={classes.card} elevation={7}>
           <CardContent>
             <Typography color="primary" type="headline" component="h2">{system}</Typography>
-              <Typography component="p">
-                <br/>
-                Total Breaks: {this.state.total}<br/>
-                New Breaks: {this.state.newBk}<br/>
-                Report Status: {this.state.status}
-              </Typography>
+            <br/>
+            <Table className={classes.table}><TableBody>
+              <TableRow>
+                <TableCell>Total Breaks</TableCell>
+                <TableCell numeric>{this.state.total}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>New Breaks</TableCell>
+                <TableCell numeric>{this.state.newBk}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Report Status</TableCell>
+                <TableCell numeric>{this.state.status}</TableCell>
+              </TableRow>
+            </TableBody></Table>
           </CardContent>
           <CardActions>
             <SimpleSelect passDate={this.onDateChange.bind(this)} />
